cf570a92755c79275796919727d5fe2664e31821
Pedro Pairazaman Silva <pedrotlx@gmail.com>
2012-05-27 21:57:07 -0500

    asdf

--- application/views/registro_view.php
+++ application/views/registro_view.php
@@ -20,6 +20,7 @@
 	form.universidad.disabled = false;
 	}
 	</script>
+	<script type='text/javascript' src=<?php echo base_url().'assets/js/nacimiento.js'?>></script>
 
 </head>
 
@@ -141,101 +142,24 @@
 	                'i2'    => 	'Ingeniería de Sistemas'
 	            );
 
-			/*$lista_univ = array(
-					'none'	=>	'--Selecciona una facultad--',
-					'pucp'	=>	'Pontificia Universidad Católica del Perú',
-					'uap'	=>	'Universidad Alas Peruanas',
-					'uac'	=>	'Universidad Andina del Cusco',
-					'uancv'	=>	'Universidad Andina Nestor Cáceres Velásquez',
-					'uarm'	=>	'Universidad Antonio Ruiz de Montoya',
-					'ucsm'	=>	'Universidad Católica de Santa María',
-					'uct'	=>	'Universidad Católica de Trujillo',
-					'uladech'	=>	'Universidad Católica los Ángeles de Chimbote',
-					'ucst'	=>	'Universidad Católica Santo Toribio',
-					'ucsp'	=>	'Universidad Católica San Pablo',
-					'ucss'	=>	'Universidad Católica Sedes Sapientiae',
-					'ucv'	=>	'Universidad César Vallejo',
-					'ucip'	=>	'Universidad Científica del Perú',
-					'ucs'	=>	'Universidad Científica del Sur',
-					'ucci'	=>	'Universidad Continental de Ciencias e Ingeniería',
-					'ucpmi'	=>	'Universidad Cristiana del Perú María Inmaculada',
-					'uch'	=>	'Universidad de Ciencias y Humanidades',
-					'uh'	=>	'Universidad de Huánuco',
-					'ul'	=>	'Universidad de Lima',
-					'upi'	=>	'Universidad de Piura',
-					'usmp'	=>	'Universidad de San Martín de Porres',
-					'up'	=>	'Universidad del Pacífico',
-					'ue'	=>	'Universidad ESAN',
-					'unife'	=>	'Universidad Femenina del Sagrado Corazón',
-					'uigv'	=>	'Universidad Inca Garcilaso de la Vega',
-					'ujbm'	=>	'Universidad Jaime Bausate y Meza',
-					'ujcm'	=>	'Universidad José Carlos Mariátegui',
-					'umch'	=>	'Universidad Marcelino Champagnat',
-					'unas'	=>	'Universidad Nacional Agraria de la Selva',
-					'unalm'	=>	'Universidad Nacional Agraria La Molina',
-					'unamd'	=>	'Universidad Nacional Amazónica de Madre de Dios',
-					'undac'	=>	'Universidad Nacional Daniel Alcides Carrión',
-					'unc'	=>	'Universidad Nacional de Cajamarca',
-					'une'	=>	'Universidad Nacional de Educación Enrique Guzman y Valle',
-					'unh'	=>	'Universidad Nacional de Huancavelica',
-					'unap'	=>	'Universidad Nacional de la Amazonía Peruana',
-					'unm'	=>	'Universidad Nacional de Moquegua',
-					'unp'	=>	'Universidad Nacional de Piura',
-					'unsa'	=>	'Universidad Nacional de San Agustín',
-					'unsaa'	=>	'Universidad Nacional de San Antonio Abad ',
-					'unsch'	=>	'Universidad Nacional San Cristóbal de Huamanga',
-					'unsm'	=>	'Universidad Nacional de San Martín',
-					'unt'	=>	'Universidad Nacional de Trujillo',
-					'untu'	=>	'Universidad Nacional de Tumbes',
-					'unu'	=>	'Universidad Nacional de Ucayali',
-					'unalt'	=>	'Universidad Nacional del Altiplano',
-					'unac'	=>	'Universidad Nacional del Callao',
-					'uncp'	=>	'Universidad Nacional del Centro del Perú',
-					'uns'	=>	'Universidad Nacional del Santa',
-					'unfv'	=>	'Universidad Nacional Federico Villarreal',
-					'unhv'	=>	'Universidad Nacional Hermilio Valdizán',
-					'uniap'	=>	'Universidad Nacional Intercultural de la Amazonía Peruana',
-					'unjbg'	=>	'Universidad Nacional Jorge Basadre Grohmann',
-					'unjfsc'	=>	'Universidad Nacional José Faustino Sanchez Carrión',
-					'unjma'	=>	'Universidad Nacional José María Arguedas',
-					'unmsm'	=>	'Universidad Nacional Mayor de San Marcos',
-					'unmba'	=>	'Universidad Nacional Micaela Bastidas de Apurimac',
-					'unprg'	=>	'Universidad Nacional Pedro Ruiz Gallo',
-					'unslg'	=>	'Universidad Nacional San Luis Gonzaga de Ica',
-					'unsam'	=>	'Universidad Nacional Santiago Antúnez de Mayolo',
-					'untecs'	=>	'Universidad Nacional Tecnológica del Cono Sur de Lima',
-					'untrm'	=>	'Universidad Nacional Toribio Rodríguez de Mendoza de Amazonas',
-					'udala'	=>	'Universidad Para el Desarrollo Andino Lircay Angaraes',
-					'upchi'	=>	'Universidad Particular de Chiclayo',
-					'upch'	=>	'Universidad Peruana Cayetano Heredia',
-					'upc'	=>	'Universidad Peruana de Ciencias Aplicadas',
-					'upci'	=>	'Universidad Peruana de Ciencias e Informática',
-					'upig'	=>	'Universidad Peruana de Integración Global',
-					'upla'	=>	'Universidad Peruana de las Américas',
-					'uplan'	=>	'Universidad Peruana Los Andes',
-					'upu'	=>	'Universidad Peruana Unión',
-					'upav'	=>	'Universidad Privada Abraham Valdelomar',
-					'upao'	=>	'Universidad Privada Antenor Orrego',
-					'upagu'	=>	'Universidad Privada Antonio Guillermo Urrelo',
-					'upcti'	=>	'Universidad Privada de Ciencias y Tecnología de Ica',
-					'upj'	=>	'Universidad Privada de Jaén',
-					'upm'	=>	'Universidad Privada de Moquegua',
-					'upp'	=>	'Universidad Privada de Pucallpa',
-					'upt'	=>	'Universidad Privada de Tacna',
-					'upn'	=>	'Universidad Privada del Norte',
-					'upjxxii'	=>	'Universidad Privada Juan XXII',
-					'uw'	=>	'Universidad Wiener',
-					'upnsp'	=>	'Universidad Privada Nuestra Señora de La Paz',
-					'upsjb'	=>	'Universidad Privada San Juan Bautista',
-					'upsp'	=>	'Universidad Privada San Pedro',
-					'upsb'	=>	'Universidad Privada Sergio Bernales',
-					'upt'	=>	'Universidad Privada Telesup',
-					'urp'	=>	'Universidad Ricardo Palma',
-					'usil'	=>	'Universidad San Ignacio de Loyola',
-					'uss'	=>	'Universidad Señor de Sipán',
-					'uta'	=>	'Universidad Tecnológica de los Andes',
-					'utp'		=>	'Universidad Tecnológica del Perú'
-				);*/
+			$lista_anho = array(
+					'none'	=>	'--Selecciona el año--',
+	                '2000'	=> 	'2000',
+	                '2001'	=> 	'2001',
+	                '2002'	=> 	'2002',
+	                '2003'	=> 	'2003',
+	                '2004'	=> 	'2004',
+	                '2005'	=> 	'2005',
+	                '2006'	=> 	'2006',
+	                '2007'	=> 	'2007',
+	                '2008'	=> 	'2008',
+	                '2009'	=> 	'2009',
+	                '2010'	=> 	'2010',
+	                '2011'	=> 	'2011',
+	                '2012'	=> 	'2012',
+	                'notyet'    => 	'Aún no soy miembro'
+	            );
+
 				$uni_si = array(
 				    'name'        => 'uni',
 				    'value'       => 'si',
@@ -293,21 +217,20 @@
 		</div>
 
 		<div class="control-group">
-			<label class="control-label" for="edad">Edad</label>
+			<label class="control-label" for="especialidad">Fecha de nacimiento:</label>
 			<div class="controls">
-				<?php echo form_input($edad); ?>
-				<p class="help-block"><?php echo form_error('edad'); ?></p>
+				<select id="anhoNac" onchange="ponerDias()" class="span1">
+				<script>ponerAnho();</script>
+				</select>
+				<select id="mesNac" onchange="ponerDias()" class="span2">
+				<script>ponerMes();</script>
+				</select>
+				<select id="diaNac" class="span1">
+				<script>ponerDias();</script>
+				</select>
 			</div>
 		</div>
-			
-		<div class="control-group">
-			<label class="control-label" for="mail">E-mail</label>
-			<div class="controls">
-				<?php echo form_input($mail); ?>
-				<p class="help-block"><?php echo form_error('mail'); ?></p>
-			</div>
-		</div>
-			
+
 		<div class="control-group">
 			<label class="control-label" for="telefono">Teléfono</label>
 			<div class="controls">
@@ -317,20 +240,36 @@
 		</div>
 
 		<div class="control-group">
+			<label class="control-label" for="mail">E-mail</label>
+			<div class="controls">
+				<?php echo form_input($mail); ?>
+				<p class="help-block"><?php echo form_error('mail'); ?></p>
+			</div>
+		</div>
+
+		<div class="control-group">
+			<label class="control-label" for="edad">¿En qué año ingresaste al CC Núcleo?</label>
+			<div class="controls">
+				<?php echo form_dropdown('anhoingreso', $lista_anho, 'none', $lista); ?>
+				<p class="help-block"><?php echo form_error('anhoingreso'); ?></p>
+			</div>
+		</div>
+			
+		<!--<div class="control-group">
 			<label class="control-label">¿Es usted estudiante de la UNI?</label>
 			<div class="controls">
-				<label class="radio"><?php echo form_radio($uni_si);?> SI, soy estudiante de la UNI.</label>
-				<label class="radio"><?php echo form_radio($uni_no);	?> NO.</label>
+				<label class="radio"><?php //echo form_radio($uni_si);?> SI, soy estudiante de la UNI.</label>
+				<label class="radio"><?php //echo form_radio($uni_no);	?> NO.</label>
 			</div>
 		</div>
 
 		<div class="control-group">
 			<label class="control-label" for="facultad">Selecione su facultad:</label>
 			<div class="controls">
-				<?php echo form_dropdown('facultad', $lista_facs, 'none', $lista); ?>
-				<p class="help-block"><?php echo form_error('facultad'); ?></p>
+				<?php //echo form_dropdown('facultad', $lista_facs, 'none', $lista); ?>
+				<p class="help-block"><?php //echo form_error('facultad'); ?></p>
 			</div>
-		</div>
+		</div>-->
 			
 		<div class="control-group">
 			<label class="control-label" for="universidad">Universidad</label>
